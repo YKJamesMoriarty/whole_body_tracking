61fd491606c8eb03f0061ab67c5ae84e13a9ff70
YKJamesMoriarty <2909438315@qq.com>
2026-03-02 03:43:26 +0800

    改为左-中位-鞭腿适配的mimic参数 1.修改effector_body_name为left_ankle_roll_link 2.修改skill one hot 和 active_effector_one_hot

--- source/whole_body_tracking/whole_body_tracking/tasks/tracking/mdp/observations.py
+++ source/whole_body_tracking/whole_body_tracking/tasks/tracking/mdp/observations.py
@@ -90,7 +90,7 @@ def motion_anchor_ori_b(env: ManagerBasedEnv, command_name: str) -> torch.Tensor
 # =============================================================================
 
 
-def target_relative_position(env: ManagerBasedEnv, command_name: str, effector_body_name: str = "right_ankle_roll_link") -> torch.Tensor:
+def target_relative_position(env: ManagerBasedEnv, command_name: str, effector_body_name: str = "left_ankle_roll_link") -> torch.Tensor:
     """
     (1) 目标相对位置 (3 维): [x, y, z]
     
@@ -227,7 +227,7 @@ def active_effector_one_hot(env: ManagerBasedEnv, command_name: str) -> torch.Te
     # Stage 1: 假设所有攻击都是右手出拳
     # 格式: [左手, 右手, 左脚, 右脚]
     one_hot = torch.zeros(env.num_envs, 4, device=env.device)
-    one_hot[:, 3] = 1.0  # 左手0，右手1，左脚2，右脚3
+    one_hot[:, 2] = 1.0  # 左手0，右手1，左脚2，右脚3
     
     # Stage 2 TODO: 根据以下信息动态确定活跃肢体:
     #   - 动作文件名 (例如: "cross" -> 右手, "hook_left" -> 左手)
@@ -282,7 +282,7 @@ def skill_type_one_hot(env: ManagerBasedEnv, command_name: str) -> torch.Tensor:
     
     one_hot = torch.zeros(env.num_envs, 16, device=env.device)
     # 右直拳0，右摆拳1，右低位鞭腿2，右高位鞭腿3，右脚前蹬4 .....
-    one_hot[:, 4] = 1.0  # 例如 one_hot[:, 0] = 1.0 为 直拳 (Stage 1)
+    one_hot[:, 8] = 1.0  # 例如 one_hot[:, 0] = 1.0 为 直拳 (Stage 1)
     
     # Stage 2 TODO: 从以下来源解析技能类型:
     #   - 动作文件名 (例如: "cross_right_normal" -> Cross)
